cf1c85b13d85eb6e1b94b4259a2376bca1a5deee
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-04-01 01:11:01 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { connection } = require("./configs/db");
+const cors = require("cors");
+const { userRoute } = require("./routes/user.route");
+const { authMiddleware } = require("./middlewares/auth");
+// const { notesRoute } = require("./routers/notes.routes");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send({ msg: "SERVER PAGE" });
+});
+
+app.use("/user", userRoute);
+app.use(authMiddleware);
+// app.use("/notes", notesRoute);
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log("connected to db");
+  } catch (error) {
+    console.log(error.message);
+  }
+  console.log(`server is running at port ${process.env.PORT}`);
+});
