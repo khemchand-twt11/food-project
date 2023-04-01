4f7202d4d618618841de8f6515410fe1d42db5d9
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-04-01 18:55:27 +0530

    search functionallity

--- routes/menu.route.js
+++ routes/menu.route.js
@@ -2,9 +2,20 @@ const express = require("express");
 const { menuModel } = require("../models/menu.model");
 const menuRoute = express.Router();
 
-menuRoute.get("/", async (req, res) => {
-  let menu = await menuModel.find();
-  res.send({ msg: "successfull!", data: menu });
+menuRoute.get("/:searchQuery", async (req, res) => {
+  try {
+    const searchTerm = req.params.searchQuery;
+    console.log(searchTerm);
+    let menu = await menuModel.find({
+      description: { $regex: new RegExp(searchTerm, "i") },
+    });
+    console.log(menu);
+    // let menu = await menuModel.find();
+    console.log(menu);
+    res.send({ msg: "successfull!", data: menu });
+  } catch (error) {
+    res.status(400).send({ error });
+  }
 });
 
 module.exports = { menuRoute };
