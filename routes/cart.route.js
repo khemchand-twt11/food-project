e75411f7d33d6162b4a7829a3742a2dda93414cc
khemchand-twt11 <gauravsharma6290852476@gmail.com>
2023-04-02 18:57:28 +0530

    cart model

---
+++ routes/cart.route.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { menuModel } = require("../models/menu.model");
+const { cartModel } = require("../models/cart.model");
+const cartRoute = express.Router();
+
+cartRoute.post("/", async (req, res) => {
+  try {
+    const { menuId, userId } = req.body;
+    const findCart = await cartModel.findOne({
+      userId: userId,
+      menuId: menuId,
+    });
+    if (findCart) {
+      res.status(200).send({ msg: "product Already in Cart!" });
+    } else {
+      const menuItem = await menuModel.findById(menuId);
+      const { _id, ...newMenuItem } = menuItem;
+      const cartItem = new cartModel({ userId, menuId, ...newMenuItem });
+      await cartItem.save();
+      console.log(cartItem);
+      res.status(200).send({ msg: "Product Added to the Cart!" });
+    }
+  } catch (error) {
+    res.status(400).send({ msg: error.message });
+  }
+});
+module.exports = { cartRoute };
